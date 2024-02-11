1253e7afd4010e781af8d1c88e26559d6aed5562
Salmanabdullah <salmandcc@gmail.com>
2024-02-11 19:33:55 +0100

    first commit

---
+++ caesarcipher.py
@@ -0,0 +1,56 @@
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+
+print('The Caesar cipher encrypts letters by shifting them over by a')
+print('key number. For example, a key of 2 means the letter A is')
+print('encrypted into C, the letter B encrypted into D, and so on.')
+print()
+
+# Let the user enter if they are encrypting or decrypting:
+while True:
+    print('Do you want to (e)ncrypt or (d)ecrypt?')
+    response = input('> ').lower()
+    if response.startswith('e'):
+        mode = 'encrypt'
+        break
+    elif response.startswith('d'):
+        mode = 'decrypt'
+        break
+    print('Please enter the letter e or d.')
+
+# Let the user enter the key to use:
+while True:
+    maxKey = len(SYMBOLS) - 1
+    print(f'Please enter the key (0 to {maxKey}) to use.')
+    response = input('> ').upper()
+    if not response.isdecimal():
+        continue
+    if 0 <= int(response) < len(SYMBOLS):
+        key = int(response)
+        break
+print(f'Enter the message to {mode}.')
+message = input('> ')
+
+# Caesar cipher only works on uppercase letters:
+message = message.upper()
+
+translated = ""
+
+# Encrypt/decrypt each symbol in the message:
+for symbol in message:
+    if symbol in SYMBOLS:
+        num = SYMBOLS.find(symbol)
+        if mode == 'encrypt':
+            num = num + key
+        elif mode == 'decrypt':
+            num = num - key
+        if num >= len(SYMBOLS):
+            num = num - len(SYMBOLS)
+        elif num < 0:
+            num = num + len(SYMBOLS)
+
+        translated = translated + SYMBOLS[num]
+    else:
+        translated = translated + symbol
+
+print(translated)
